8997829c6bf9631c780ab7d8cb1b47fc97d202ec
Win-10 <azimovanvarjon777@gmail.com>
2024-11-30 14:29:24 +0100

    created user_auth_page

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:user_profile_auth/auth/auth.dart';
+import 'package:user_profile_auth/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthPage(),
+    );
+  }
+}
